4b0f1d1b4b5f32ace6775150eea9439c8433f512
laiscalafange <laiscalafange@gmail.com>
2026-04-10 15:01:41 -0300

    inicio pedometer

--- hooks/usePedometer.jsx
+++ hooks/usePedometer.jsx
@@ -1,2 +1,21 @@
 import { useState, useEffect } from 'react';
+import { Pedometer } from 'expo-sensors';
 
+
+export default function usePedometer() {
+  const [passos, setPassos] = useState(0);  
+  const [isAvailable, setIsAvailable] = useState(false); // Verifica se o celular tem esse sensor
+
+    useEffect(() => {
+    let subscription;
+
+  
+
+  }, []);
+
+  // Entrega os dados pro app
+  return {
+    passos,
+    isAvailable
+  };
+}
