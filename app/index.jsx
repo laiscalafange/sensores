26ebb3baa5fc8db06a5881c0296f8bc077dea62e
laiscalafange <laiscalafange@gmail.com>
2026-04-15 10:17:38 -0300

    deixando interface no meio

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
 import useBussola from '../hooks/useMagnet';
 import usePedometer from '../hooks/usePedometer';
-
 
 export default function App() {
   const { graus, direcao } = useBussola();
   const { passosFiltrados } = usePedometer();
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Bússola</Text>
       <Text>Graus: {graus}°</Text>
-      <Text>Direção: {direcao}</Text>
+      <Text>Direção: {direcao}{"\n"}</Text>
 
       <Text>Pedômetro</Text>
-      <Text>Passos: { passosFiltrados }</Text>
+      <Text>Passos: {passosFiltrados}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // ocupa a tela toda
+    justifyContent: 'center', // centraliza verticalmente
+    alignItems: 'center', // centraliza horizontalmente
+  }
+});
