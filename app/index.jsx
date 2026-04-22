b422a45300fee75538683f641db2fbf225d16a9f
jujuli2 <juliachaves402@gmail.com>
2026-04-21 22:44:22 -0300

    vaiii
    prfvvv

--- app/index.jsx
+++ app/index.jsx
@@ -5,24 +5,17 @@ import usePedometer from '../hooks/usePedometer';
 
 export default function App() {
   const { graus, direcao } = useBussola();
-  const { passosFiltrados } = usePedometer();
+  const { steps } = usePedometer();
 
   return (
-    <View style={styles.container}>
+    <View >
       <Text>Bússola</Text>
       <Text>Graus: {graus}°</Text>
       <Text>Direção: {direcao}{"\n"}</Text>
 
       <Text>Pedômetro</Text>
-      <Text>Passos: {passosFiltrados}</Text>
+      <Text>Passos: {steps}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, // ocupa a tela toda
-    justifyContent: 'center', // centraliza verticalmente
-    alignItems: 'center', // centraliza horizontalmente
-  }
-});
